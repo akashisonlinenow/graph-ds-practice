dd87c0d973bb8111fad174a8c71fa876ea83306b
Akash Shridharan <akashshridharan2802@gmail.com>
2023-03-24 11:38:56 +0530

    unionBySize Added

--- DisjointSet_G46.cpp
+++ DisjointSet_G46.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
 public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
+        size.resize(n + 1);
         for (int i = 0; i <= n; i++)
         {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -49,16 +51,37 @@ public:
             rank[ulp_u]++;
         }
     }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v)
+        {
+            return;
+        }
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += ulp_v;
+        }
+    }
 };
 
 int main()
 {
-    DisjointSet ds(7);
-    ds.unionByRank(1, 2);
-    ds.unionByRank(2, 3);
-    ds.unionByRank(4, 5);
-    ds.unionByRank(6, 7);
-    ds.unionByRank(5, 6);
+    DisjointSet ds(7); // note-> dont use unionBySize and UnionByRank together here...
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
     if (ds.findUPar(3) == ds.findUPar(7))
     { // not same will be o/p
         cout << "Same"
@@ -70,7 +93,7 @@ int main()
              << "\n";
     }
 
-    ds.unionByRank(3, 7);
+    ds.unionBySize(3, 7);
 
     if (ds.findUPar(3) == ds.findUPar(7)) //  same will be o/p
     {
