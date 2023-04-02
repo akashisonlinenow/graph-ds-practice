8a98dfba5d48244f1fd685b0e17544b2f399d3b7
Akash Shridharan <akashshridharan2802@gmail.com>
2023-04-02 12:16:35 +0530

    modified

--- CriticalConnectionsInNetwok_Bridges_TarjansAlgo.cpp
+++ CriticalConnectionsInNetwok_Bridges_TarjansAlgo.cpp
@@ -1,134 +1,70 @@
-<<<<<<< HEAD
-// leetcode - Critical connections in a network
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution
 {
-
 private:
     int timer = 1;
-
-private:
-    void dfs(int node, int parent, vector<int> &vis, vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges)
+    void dfs(int node, int parent, vector<int> &vis,
+             vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges)
     {
         vis[node] = 1;
         tin[node] = low[node] = timer;
         timer++;
-
         for (auto it : adj[node])
         {
-            // if adj node is parent dont do anything
             if (it == parent)
                 continue;
-
-            // if its unvisited call dfs
-            if (!vis[it])
+            if (vis[it] == 0)
             {
                 dfs(it, node, vis, adj, tin, low, bridges);
-                // after dfs of it completes it comes back, then node says whats your lowest step give it to me
-                low[node] = min(low[node], low[it]);
-
-                // can this node --> it be a bridge?
+                low[node] = min(low[it], low[node]);
+                // node --- it
                 if (low[it] > tin[node])
-                { // then it is a bridge
+                {
                     bridges.push_back({it, node});
                 }
             }
-
-            // if its visited
             else
-            { // then it cannot be a bridge just take low of node
+            {
                 low[node] = min(low[node], low[it]);
             }
         }
     }
 
 public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>> &connections)
+    vector<vector<int>> criticalConnections(int n,
+                                            vector<vector<int>> &connections)
     {
-        // refer striver
-
-        // graph creation -> undirected to directed
         vector<int> adj[n];
         for (auto it : connections)
         {
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+            int u = it[0], v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-
         vector<int> vis(n, 0);
         int tin[n];
         int low[n];
         vector<vector<int>> bridges;
-
         dfs(0, -1, vis, adj, tin, low, bridges);
-
         return bridges;
     }
-=======
-// leetcode - Critical connections in a network
-
-class Solution
-{
-
-private:
-    int timer = 1;
-
-private:
-    void dfs(int node, int parent, vector<int> &vis, vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges)
-    {
-        vis[node] = 1;
-        tin[node] = low[node] = timer;
-        timer++;
-
-        for (auto it : adj[node])
-        {
-            // if adj node is parent dont do anything
-            if (it == parent)
-                continue;
-
-            // if its unvisited call dfs
-            if (!vis[it])
-            {
-                dfs(it, node, vis, adj, tin, low, bridges);
-                // after dfs of it completes it comes back, then node says whats your lowest step give it to me
-                low[node] = min(low[node], low[it]);
-
-                // can this node --> it be a bridge?
-                if (low[it] > tin[node])
-                { // then it is a bridge
-                    bridges.push_back({it, node});
-                }
-            }
-
-            // if its visited
-            else
-            { // then it cannot be a bridge just take low of node
-                low[node] = min(low[node], low[it]);
-            }
-        }
-    }
-
-public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>> &connections)
-    {
-        // refer striver
-
-        // graph creation -> undirected to directed
-        vector<int> adj[n];
-        for (auto it : connections)
-        {
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-
-        vector<int> vis(n, 0);
-        int tin[n];
-        int low[n];
-        vector<vector<int>> bridges;
-
-        dfs(0, -1, vis, adj, tin, low, bridges);
-
-        return bridges;
-    }
->>>>>>> 352036d788ee7035b34b57c9a165a5361f3d037f
 };
+
+int main()
+{
+
+    int n = 4;
+    vector<vector<int>> connections = {
+        {0, 1}, {1, 2}, {2, 0}, {1, 3}};
+
+    Solution obj;
+    vector<vector<int>> bridges = obj.criticalConnections(n, connections);
+    for (auto it : bridges)
+    {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
+    return 0;
+}
